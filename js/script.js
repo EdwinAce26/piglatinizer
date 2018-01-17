ecb9d96b39bcb5562b9d017706f7b9f2f798b615
Edwin <edwin.j.acevedo.01@gmail.com>
2018-01-17 23:12:09 +0000

    attempting the .includes

--- js/script.js
+++ js/script.js
@@ -6,22 +6,34 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $( document ).ready(function() {
-  
-
-
-
-});
-
-
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
-
-
-
-
-
-
-// Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-
+    $("#latinize").click(function(){
+       
+        var vowel = ["a","e","i","o","u","y"]
+            function translate (words){
+                
+                words = $("#input").val();
+                    console.log(words);
+                
+                var splitWords = words.split("");
+                    console.log(splitWords);
+                
+                var include = splitWords.includes(vowel);    
+                    console.log(include);
+                    
+               if(include === true){
+                    
+                    $("#results").text(words + "ay");
+                    console.log()
+                }
+            }
+            
+        var response = translate();
+        
+        //console.log(response);
+    // Create the sentenceToPigLatin function that takes a sentence as a parameter
+    	//Loops through all the words in the sentence and transforms each word
+    	//It should return a transfromed sentance
+    
+    });
+});
